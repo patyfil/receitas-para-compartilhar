2b07f9220e45b13becbd20a8bc3f5184afa6e444
patyfil <patyfil@gmail.com>
2023-11-08 11:16:49 -0300

    .venv

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
