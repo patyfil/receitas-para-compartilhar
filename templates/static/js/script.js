1eaa48016ed8e64f66662b91dd6d5c3aa15176d6
patyfil <patyfil@gmail.com>
2023-11-14 16:00:42 -0300

    alt css

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,66 +1,70 @@
-const switchLabel = document.querySelector('.switch');
-const loginForm = document.getElementById('login-form');
-const cadastroForm = document.getElementById('cadastro-form');
+document.addEventListener('DOMContentLoaded', function () {
+    const switchInput = document.getElementById('log');
+    const loginForm = document.getElementById('login-form');
+    const cadastroForm = document.getElementById('cadastro-form');
 
-// Função para alternar o formulário ativo
-function toggleForm() {
-    const isLoginFormActive = loginForm.style.display === 'block';
+    // Função para alternar o formulário ativo
+    function toggleForm() {
+        const isCadastroFormActive = switchInput.checked;
 
-    if (isLoginFormActive) {
-        loginForm.style.display = 'none';
-        cadastroForm.style.display = 'block';
-    } else {
-        loginForm.style.display = 'block';
-        cadastroForm.style.display = 'none';
+        if (isCadastroFormActive) {
+            loginForm.style.display = 'none';
+            cadastroForm.style.display = 'block';
+        } else {
+            loginForm.style.display = 'block';
+            cadastroForm.style.display = 'none';
+        }
     }
-}
 
-// Defina o estado inicial com base na posição do switch
-if (switchLabel.classList.contains('active')) {
-    loginForm.style.display = 'none';
-    cadastroForm.style.display = 'block';
-} else {
-    loginForm.style.display = 'block';
-    cadastroForm.style.display = 'none';
-}
-
-// Adicione um ouvinte de evento para alternar o formulário quando o switch é alterado
-switchLabel.addEventListener('change', toggleForm);
-
-
-// Password eyes - Digitação de senha (icone)
-function togglePasswordVisibility(icon) {
-    var passwordInput = icon.previousElementSibling;
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        icon.classList.remove("fa-eye-slash");
-        icon.classList.add("fa-eye");
-    } else {
-        passwordInput.type = "password";
-        icon.classList.remove("fa-eye");
-        icon.classList.add("fa-eye-slash");
+    // Adicione um ouvinte de evento para alternar o formulário quando o switch é alterado
+    if (switchInput) {
+        switchInput.addEventListener('change', toggleForm);
     }
-}
 
+    // Defina o estado inicial com base na posição do switch
+    toggleForm();
 
-// Digitação do e-mail para lowercase
-const emailInputEntrar = document.getElementById("emailInputEntrar");
-const emailInputCadastrar = document.getElementById("emailInputCadastrar");
+    // Digitação do e-mail para lowercase
+    const emailInputEntrar = document.getElementById("emailInputEntrar");
+    const emailInputCadastrar = document.getElementById("emailInputCadastrar");
 
-// Função para converter o texto do campo de e-mail em minúsculas
-function convertToLowerCase(event) {
-    event.target.value = event.target.value.toLowerCase();
-}
+    // Função para converter o texto do campo de e-mail em minúsculas
+    function convertToLowerCase(event) {
+        event.target.value = event.target.value.toLowerCase();
+    }
 
-// Adicione um ouvinte de evento para o campo de entrada de e-mail no formulário de login
-emailInputEntrar.addEventListener("input", convertToLowerCase);
+    // Adicione um ouvinte de evento para o campo de entrada de e-mail no formulário de login
+    if (emailInputEntrar) {
+        emailInputEntrar.addEventListener("input", convertToLowerCase);
+    }
 
-// Adicione um ouvinte de evento para o campo de entrada de e-mail no formulário de cadastro
-emailInputCadastrar.addEventListener("input", convertToLowerCase);
+    // Adicione um ouvinte de evento para o campo de entrada de e-mail no formulário de cadastro
+    if (emailInputCadastrar) {
+        emailInputCadastrar.addEventListener("input", convertToLowerCase);
+    }
 
+    // Adicione ouvinte de evento para a senha no formulário de cadastro
+    if (senhaInputCadastrar) {
+        senhaInputCadastrar.addEventListener("input", function () {
+            checkPassword(senhaInputCadastrar);
+            checkPassword(senhaConfirmInput);
+        });
+    }
+
+    // Adicione ouvinte de evento para a confirmação de senha no formulário de cadastro
+    if (senhaConfirmInput) {
+        senhaConfirmInput.addEventListener("input", function () {
+            checkPassword(senhaInputCadastrar);
+            checkPassword(senhaConfirmInput);
+        });
+    }
+});
 
 
 // Checar senha
+const senhaInputCadastrar = document.getElementById("senhaInputCadastrar");
+const senhaConfirmInput = document.getElementById("confirmarSenhaInput");
+
 function checkPassword(input) {
     const password = input.value;
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{6,12}$/;
